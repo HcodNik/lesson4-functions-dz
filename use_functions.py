f24026309af6ec670b2011152907d944e3995934
HcodNik <bruchikhin@gmail.com>
2023-11-14 15:02:22 +0300

    lesson

--- use_functions.py
+++ use_functions.py
@@ -1,4 +1,5 @@
-
+import datetime
+from datetime import date
 """
 МОДУЛЬ 3
 Программа "Личный счет"
@@ -35,23 +36,73 @@
 Для реализации основного меню можно использовать пример ниже или написать свой
 """
 
-while True:
-    print(f'1. пополнение счета')
-    print('2. покупка')
-    print('3. история покупок')
-    print('4. выход')
+wallet = 5000.0
+shopping_book = []
 
-    choice = input('Выберите пункт меню: ')
-    if choice == '1':
-        pass
-    elif choice == '2':
-        pass
-    elif choice == '3':
-        pass
-    elif choice == '4':
-        break
+def curent_date(): # текущая дата
+    current_date = str(date.fromisoformat('2019-01-01').today())
+    return current_date
+def adding_wallet_func(): # пополнение счёта
+    global wallet
+    add_money = input('Введите сумму пополнения в рублях: ')
+    while add_money.isdigit() != True:
+        print('Некоректный ввод! Повторите попытку.')
+        return adding_wallet_func()
+    wallet += float(add_money)
+    print(f'Пополнение на сумму {float(add_money)} руб. выполнено успешно.')
+    return menu_func()
+
+def buy_func(): # Покупка
+    global wallet
+    purchase_sum = input('Введите сумму покупки в рублях: ')
+    while purchase_sum.isdigit() != True:
+        print('Некоректный ввод! Повторите попытку.')
+        return buy_func()
+    if float(purchase_sum) > wallet:
+        print('Недостаточно средств!')
+        menu_func()
+    elif float(purchase_sum) == 0:
+        print('Сумма не может быть равнв нулю!')
+        menu_func()
     else:
-        print('Неверный пункт меню')
+        purchase_name = input('Введите название покупки: ')
+        wallet -= float(purchase_sum)
+        history_func(purchase_name, purchase_sum)
+        return menu_func()
+
+def history_func(purchase_name, purchase_sum): # история покупок
+    global shopping_book
+    current_purchase = [curent_date(), purchase_name, purchase_sum]
+    shopping_book.append(current_purchase)
+    return shopping_book
+
+
+def menu_func(): # главное меню
+    while True:
+        print(f'Текущий баланс: {wallet} руб.')
+        print(f'1. пополнение счета.')
+        print('2. покупка')
+        print('3. история покупок')
+        print('4. выход')
+
+        choice = input('Выберите пункт меню: ')
+        if choice == '1':
+            adding_wallet_func()
+        elif choice == '2':
+            buy_func()
+        elif choice == '3':
+            if shopping_book == []:
+                print('История пуста...')
+            print('Дата.....................Сумма..............Наименование операции')
+            for i in range(len(shopping_book)):
+                print(f'{shopping_book[i][0]}...............{shopping_book[i][2]}...............{shopping_book[i][1]}')
+            menu_func()
+        elif choice == '4':
+            break
+        else:
+            print('Неверный пункт меню')
+
+menu_func()
 
 
 
