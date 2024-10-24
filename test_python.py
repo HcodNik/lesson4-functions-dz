b46e226cb62582b9823aede21cbb41de57a95387
HcodNik <bruchikhin@gmail.com>
2024-10-24 17:28:31 +0300

    Добавлен тест map()

---
+++ test_python.py
@@ -0,0 +1,13 @@
+
+"""
+В модуле написать тесты для встроенных функций filter, map, sorted, а также для функций из
+библиотеки math: pi, sqrt, pow, hypot. Чем больше тестов на каждую функцию - тем лучше
+"""
+
+# Тестирование функции map()
+def test_map():
+    assert list(map(lambda x: x * 2, [1, 2])) == [2, 4]
+    assert list(map(lambda x: x * 2, ['Max', 'Leo'])) == ['MaxMax', 'LeoLeo']
+
+
+
