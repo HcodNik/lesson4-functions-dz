f1d8784b904e49d84ea7791bcb8970020cc10bdd
HcodNik <bruchikhin@gmail.com>
2024-10-24 17:33:00 +0300

    Добавлен тест test_map_func ()

--- test_python.py
+++ test_python.py
@@ -5,7 +5,7 @@
 """
 
 # Тестирование функции map()
-def test_map():
+def test_map_func():
     assert list(map(lambda x: x * 2, [1, 2])) == [2, 4]
     assert list(map(lambda x: x * 2, ['Max', 'Leo'])) == ['MaxMax', 'LeoLeo']
 
